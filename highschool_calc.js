ba8e77fdd6698a3088b64150abb7024f29f568c9
Sen2017 <casintahan47at@gmail.com>
2016-11-21 08:41:32 -1000

    completed

--- highschool_calc.js
+++ highschool_calc.js
@@ -1,18 +1,18 @@
 function calculator (a,b,c){
-	if (a == "add"){
-		console.log(b + c);
+	if (b == "+"){
+		console.log(a + c);
 	}
-	else if (a == "subtract"){
-		console.log(b - c);
+	else if (b == "-"){
+		console.log(a - c);
 	}
-	else if (a == "divide"){
-		console.log(b / c);
+	else if (b == "/"){
+		console.log(a / c);
 	}
-	else if (a == "multiply"){
-		console.log(b * c);
+	else if (b == "x"){
+		console.log(a * c);
 	}else{
 		console.log("I actually suck at math LOL");
 	}
 }
 
-calculator("multiply", 5, 5);
+calculator(5, "x", 5);
