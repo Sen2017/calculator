8308d4709120cdd83d9ff902070957982f9e5186
Sen2017 <casintahan47at@gmail.com>
2016-11-21 08:34:36 -1000

    complete

--- highschool_calc.js
+++ highschool_calc.js
@@ -1,1 +1,18 @@
-function calculator(a,b,c);
+function calculator (a,b,c){
+	if (a == "add"){
+		console.log(b + c);
+	}
+	else if (a == "subtract"){
+		console.log(b - c);
+	}
+	else if (a == "divide"){
+		console.log(b / c);
+	}
+	else if (a == "multiply"){
+		console.log(b * c);
+	}else{
+		console.log("I actually suck at math LOL");
+	}
+}
+
+calculator("multiply", 5, 5);
